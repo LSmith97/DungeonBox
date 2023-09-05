afa454bdcf2ae38e8e59537618b8f6519b8f6bec
lsmith <therealshaunsred@gmail.com>
2023-09-05 10:15:22 -0400

    updated controllers

--- backend/controllers/characters.js
+++ backend/controllers/characters.js
@@ -1,3 +1,10 @@
+///////////////////////////////
+// DEPENDENCIES
+////////////////////////////////
+
+const express = require('express')
+const {Character} = require('../models')
+
 module.exports = {
     index,
     show,
@@ -6,22 +13,42 @@ module.exports = {
     update,
 }
 
-async function index(req, res) {
-
+async function index(req, res, next) {
+    try {
+        res.json(await Character.find({}));
+    } catch (error) {
+        res.status(400).json(error);
+    }
 }
 
 async function show(req, res) {
-    
+    try {
+        res.json(await Character.findById(req.params.id));
+    } catch (error) {
+        res.status(400).json(error);
+    }
 }
 
 async function create(req, res) {
-    
+    try {
+        res.json(await Character.create(req.body));
+    } catch (error) {
+        res.status(400).json(error);
+    }
 }
 
 async function destroy(req, res) {
-    
+    try {
+        res.json(await Character.findByIdAndRemove(req.params.id));
+    } catch (error) {
+        res.status(400).json(error);
+    }
 }
 
 async function update(req, res) {
-    
+    try {
+        res.json(await Character.findByIdAndUpdate(req.params.id, req.body, {new: true}));
+    } catch (error) {
+        res.status(400).json(error);
+    }
 }

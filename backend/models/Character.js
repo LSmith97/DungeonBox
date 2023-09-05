f667dfe1f1d66c6db6b26349c9d8e6f073cad9c0
lsmith <therealshaunsred@gmail.com>
2023-09-05 10:05:22 -0400

    created schema for character model

---
+++ backend/models/Character.js
@@ -0,0 +1,71 @@
+///////////////////////////////
+// DEPENDENCIES
+////////////////////////////////
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+
+///////////////////////////////
+// MODELS
+////////////////////////////////
+
+const CharacterSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: String,
+  class: {
+    type: String,
+    required: true,
+  },
+  level: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 20
+  },
+  race: {
+    type: String,
+    required: true,
+  },
+  str: {
+    type: Number,
+    required: true,
+    min: 6,
+    max: 20
+  },
+  dex: {
+    type: Number,
+    required: true,
+    min: 6,
+    max: 20
+  },
+  con: {
+    type: Number,
+    required: true,
+    min: 6,
+    max: 20
+  },
+  int: {
+    type: Number,
+    required: true,
+    min: 6,
+    max: 20
+  },
+  wis: {
+    type: Number,
+    required: true,
+    min: 6,
+    max: 20
+  },
+  cha: {
+    type: Number,
+    required: true,
+    min: 6,
+    max: 20
+  }
+},{timestamps: true});
+
+module.exports = mongoose.model("Character", CharacterSchema);
+

f9df10a041774c6839ed8331d46bddcddf8e342f
lsmith <therealshaunsred@gmail.com>
2023-09-05 09:39:33 -0400

    Stubbed up controller

---
+++ backend/controllers/people.js
@@ -0,0 +1,32 @@
+module.exports = {
+    index,
+    show,
+    new: newCharacter,
+    create,
+    destroy,
+    edit,
+}
+
+async function index(req, res) {
+
+}
+
+async function show(req, res) {
+    
+}
+
+async function newCharacter(req, res) {
+    
+}
+
+async function create(req, res) {
+    
+}
+
+async function destroy(req, res) {
+    
+}
+
+async function edit(req, res) {
+    
+}

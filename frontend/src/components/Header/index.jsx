f98ba627513c4d948795b9c7f7d7021dbfeb58a4
lsmith <therealshaunsred@gmail.com>
2023-09-05 12:30:37 -0400

    Cleaned up react

---
+++ frontend/src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import Nav from "./Nav"
+
+function Header() {
+    return (
+      <header>
+        <h1>DungeonBox</h1>
+        <Nav />
+      </header>
+    );
+  };
+  
+  export default Header;

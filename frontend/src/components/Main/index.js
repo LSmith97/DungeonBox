1a1be53e49877afec3f1d13403ae7721aebde669
lsmith <therealshaunsred@gmail.com>
2023-09-05 14:14:57 -0400

    added routing to main

--- frontend/src/components/Main/index.js
+++ frontend/src/components/Main/index.js
@@ -1,10 +1,19 @@
-import Button from '@mui/material/Button';
+import { Routes, Route } from 'react-router-dom';
+import Home from "../../pages/Home";
+import Show from "../../pages/Show";
+import Characters from "../../pages/Characters"
+import New from "../../pages/New"
 
 export default function Main(){
     return(
-        <div>
-            
-        </div>
+        <main>
+            <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/characters" element={<Characters />} />
+            <Route path="/characters/new" element={<New />} />
+            <Route path="/characters/:id" element={<Show />} />
+            </Routes>
+        </main>
     )
 }
 
---
+++ frontend/src/pages/Characters/index.js
@@ -0,0 +1,5 @@
+export default function Characters(){
+    return(
+        <h1>Index</h1>
+    )
+}

9d96f6b0325f9f530b0412b4c042419cb7ebb641
lsmith <therealshaunsred@gmail.com>
2023-09-07 14:44:11 -0400

    Auth0 implementation

--- frontend/src/components/Header/Nav.jsx
+++ frontend/src/components/Header/Nav.jsx
@@ -1,7 +1,13 @@
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
+import LoginButton from "../Auth/LoginButton";
+import LogoutButton from "../Auth/LogoutButton";
+import { useAuth0 } from "@auth0/auth0-react"
 
 export default function Nav() {
+
+  const { user } = useAuth0()
+
   return (
     <nav className="Nav">
       <Link to="/">
@@ -13,6 +19,7 @@ export default function Nav() {
       <Link to="/characters/new">
         <Button variant="contained">New Character</Button>
       </Link>
+      {user ? <LogoutButton /> : <LoginButton />}
     </nav>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import Auth0ProviderWithNavigate from "./components/Auth/AuthProvider";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -11,7 +13,11 @@ import "@fontsource/roboto/700.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <React.StrictMode>
+    <Router>
+      <Auth0ProviderWithNavigate>
+        <App />
+      </Auth0ProviderWithNavigate>
+    </Router>
+  </React.StrictMode>
 );
